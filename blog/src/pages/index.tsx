cd5f60af8494653b7f330da39a894c7bc8b55def
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-05-16 23:18:13 +0900

    Test graphql query

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import Text from 'components/Test';
+import Text from 'components/Text';
 
 const IndexPage: FunctionComponent = function () {
   return <Text text="Home" />;
