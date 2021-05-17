74d2a416210a39eca4e47ca5608f0cca1cf2849e
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-05-18 00:05:59 +0900

    Implement CategoryList
    Use props to make category list

--- blog/src/pages/index.tsx
+++ blog/src/pages/index.tsx
@@ -1,13 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import GlobalStyle from 'components/Common/GlobalStyle';
-import Introduction from 'components/Main/Introduction';
 import Footer from 'components/Common/Footer';
+import CategoryList from 'components/Main/CategoryList';
+import Introduction from 'components/Main/Introduction';
+
+const CATEGORY_LIST = {
+  All: 5,
+  Web: 3,
+  Mobile: 2,
+};
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100%;
+  height: 100vh;
 `;
 
 const IndexPage: FunctionComponent = function () {
@@ -15,6 +22,7 @@ const IndexPage: FunctionComponent = function () {
     <Container>
       <GlobalStyle />
       <Introduction />
+      <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
       <Footer />
     </Container>
   );
