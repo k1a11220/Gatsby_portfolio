8b0bda83cc616dabf561b6c4a0c57e2e7ca63ed1
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-05-26 00:01:30 +0900

    Implement responsive layout

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/beomsoo/Documents/GitHub/blog/.cache/dev-404-page.js")),
+  "component---src-pages-index-tsx": preferDefault(require("/Users/beomsoo/Documents/GitHub/blog/src/pages/index.tsx")),
+  "component---src-pages-info-tsx": preferDefault(require("/Users/beomsoo/Documents/GitHub/blog/src/pages/info.tsx"))
+}
+
