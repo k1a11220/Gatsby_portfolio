d8c15fd30578f6a7f687cfdaabf1cd48a7214c27
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-05-26 00:25:56 +0900

    Implement export post data

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,4 +1,7 @@
 module.exports = [{
+      plugin: require('../node_modules/gatsby-remark-images/gatsby-browser.js'),
+      options: {"plugins":[],"maxWidth":768,"quality":100,"withWebp":true,"linkImagesToOriginal":true,"showCaptions":false,"markdownCaptions":false,"sizeByPixelDensity":false,"backgroundColor":"white","tracedSVG":false,"loading":"lazy","disableBgImageOnAlpha":false,"disableBgImage":false},
+    },{
       plugin: require('../node_modules/gatsby-plugin-gatsby-cloud/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import React, { FunctionComponent } from 'react';
-import styled from '@emotion/styled';
-import GlobalStyle from 'components/Common/GlobalStyle';
-import Footer from 'components/Common/Footer';
-import CategoryList from 'components/Main/CategoryList';
-import Introduction from 'components/Main/Introduction';
-import PostList from 'components/Main/PostList';
+import React, { FunctionComponent } from "react";
+import styled from "@emotion/styled";
+import GlobalStyle from "components/Common/GlobalStyle";
+import Footer from "components/Common/Footer";
+import CategoryList from "components/Main/CategoryList";
+import Introduction from "components/Main/Introduction";
+import PostList, { PostType } from "components/Main/PostList";
+import { graphql } from "gatsby";
+
+interface IndexPageProps {
+  data: {
+    allMarkdownRemark: {
+      edges: PostType[];
+    };
+  };
+}
 
 const CATEGORY_LIST = {
   All: 5,
@@ -17,17 +26,43 @@ const Container = styled.div`
   flex-direction: column;
   height: 100%;
 `;
-
-const IndexPage: FunctionComponent = function () {
+const IndexPage: FunctionComponent<IndexPageProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
   return (
     <Container>
       <GlobalStyle />
       <Introduction />
       <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-      <PostList />
+      <PostList posts={edges} />
       <Footer />
     </Container>
   );
 };
 
 export default IndexPage;
+
+export const queryPostList = graphql`
+  query queryPostList {
+    allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date, frontmatter___title] }
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            summary
+            date(formatString: "YYYY.MM.DD.")
+            categories
+            thumbnail {
+              publicURL
+            }
+          }
+        }
+      }
+    }
+  }
+`;
