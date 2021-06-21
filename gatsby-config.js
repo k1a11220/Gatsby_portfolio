e47cd6ae3a9c7053a258f0da6bc262d3a6249002
Beomsoo <44296964+k1a11220@users.noreply.github.com>
2021-06-22 01:32:55 +0900

    Deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -89,7 +89,7 @@ module.exports = {
           {
             resolve: "gatsby-plugin-canonical-urls",
             options: {
-              siteUrl: "<https://blog.beomsoo.me/>",
+              siteUrl: "https://blog.beomsoo.me/",
               stripQueryString: true,
             },
           },
